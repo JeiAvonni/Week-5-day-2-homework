9a251fa57f27ef6790a8cdcecb4f9e88541b80b7
jeiavonni <jeiavonni765@gmail.com>
2024-02-02 16:10:40 -0600

    Week 5 day 2

---
+++ Week5Day2Queries.sql
@@ -0,0 +1,8 @@
+-- query for first_name and last_name from the actor table
+SELECT first_name, last_name
+FROM actor;
+
+-- Query for a first_name that equals Nick using the WHERE clause
+SELECT first_name, last_name
+FROM actor
+WHERE first_name = 'Nick';
